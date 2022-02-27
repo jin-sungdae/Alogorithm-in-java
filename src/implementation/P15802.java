33f052016fbdbb6ebacd138be3f50aadd49050ef
Sungdae Jin <sjin@c6r8s5.42seoul.kr>
2022-02-27 21:54:47 +0900

    implement baekjoon sliver 2

---
+++ src/implementation/P15802.java
@@ -0,0 +1,20 @@
+package implementation;
+
+
+import java.util.*;
+import java.io.*;
+
+public class P15802 {
+	public static void main(String[]args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+
+		if (str.equals("innovation"))
+			System.out.println("1");
+		else
+			System.out.println("0");
+	}
+}
