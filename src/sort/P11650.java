a06b865b3a410a39ae6fd97aeaf09291b498db58
jin-sungdae <makonle@naver.com>
2022-03-04 02:48:59 +0900

    sort

---
+++ src/sort/P11650.java
@@ -0,0 +1,34 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P11650 {
+    public static void main(String [] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int [][] alist =new int[N][2];
+
+        for (int i = 0; i < N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            alist[i][0] = a;
+            alist[i][1] = b;
+
+        }
+        Arrays.sort(alist, (e1, e2)->{
+            if (e1[0] == e2[0]){
+                return e1[1] - e2[1];
+            } else{
+                return e1[0] - e2[0];
+            }
+        });
+        for (int i = 0; i < N; i++){
+            System.out.println(alist[i][0] + " " + alist[i][1]);
+        }
+
+    }
+}
