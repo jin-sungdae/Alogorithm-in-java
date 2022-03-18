d71e36943aef7063c10e858c01bb4efaefb551ab
jin-sungdae <makonle@naver.com>
2022-03-18 16:00:38 +0900

    P17413

--- src/string/P17413.java
+++ src/string/P17413.java
@@ -1,26 +1,52 @@
 package string;
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class P17413 {
-	
-	public static String solution(String str) {
-		String [] strResult = str.split(" ");
-		String result = "";
-		for (int i = 0; i < strResult.length; i++) {
-			result += new StringBuilder(strResult[i]).reverse().toString();
-			result += " ";
-		}
-		return result;
-	}
-	
-	public static void main(String []args) throws IOException{
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		String str = br.readLine();
-		if (str.contains("<")) {
-			str = str.substring(str.indexOf(">") + 1, str.lastIndexOf("<"));
+		StringBuilder sb = new StringBuilder();
+
+		boolean flag = false;
+
+		Stack < Character > stack = new Stack < > ();
+
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == '<') {
+				while (!stack.isEmpty()) {
+					sb.append(stack.pop());
+				}
+				flag = true;
+			}
+			else if (str.charAt(i) == '>') {
+				flag = false;
+				sb.append(str.charAt(i));
+				continue;
+			}
+
+			if (flag == true) {
+				sb.append(str.charAt(i));
+			}
+			else if (flag == false) {
+				if (str.charAt(i) == ' ') {
+					while (!stack.isEmpty()) {
+						sb.append(stack.pop());
+					}
+					sb.append(' ');
+					continue;
+				} else {
+					stack.push(str.charAt(i));
+				}
+			}
+			if (i == str.length() - 1) {
+				while (!stack.isEmpty()) {
+					sb.append(stack.pop());
+				}
+			}
 		}
-		System.out.println(solution(str));
+		System.out.println(sb);
 	}
 }
