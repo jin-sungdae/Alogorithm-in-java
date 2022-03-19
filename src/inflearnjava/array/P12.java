d3ab4e689edfddcb34990dad57ec0667c1635a53
Sungdae Jin <sjin@c6r8s7.42seoul.kr>
2022-03-19 22:10:59 +0900

    array All:

---
+++ src/inflearnjava/array/P12.java
@@ -0,0 +1,43 @@
+package inflearnjava.array;
+
+import java.util.*;
+
+public class P12 {
+	
+	public static void solution(int [][] list, int N, int M) {
+		int pi = 0, pj = 0, result = 0, cnt =0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				cnt = 0;
+				for (int k = 0; k < M; k++) {
+					for (int s = 0; s < N; s++) {
+						if (list[k][s] == i) {
+							pi = s;
+						}
+						if (list[k][s] == j) {
+							pj = s;
+						}
+					}
+					if (pi > pj)
+						cnt++;
+				}
+				if (cnt == M)
+					result++;
+			}
+		}
+		System.out.println(result);
+	}
+	
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int [][] list = new int[M][N];
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				list[i][j] = sc.nextInt();
+			}
+		}
+		solution(list, N, M);
+	}
+}
