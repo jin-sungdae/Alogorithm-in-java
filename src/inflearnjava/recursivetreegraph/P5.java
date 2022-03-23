59317ccf976dbe205ffe6d7fbf3766a2d4dcc223
Sungdae Jin <sjin@c10r3s6.42seoul.kr>
2022-03-23 20:43:43 +0900

    BFS

--- src/inflearnjava/recursivetreegraph/P5.java
+++ src/inflearnjava/recursivetreegraph/P5.java
@@ -1,4 +1,37 @@
 package inflearnjava.recursivetreegraph;
 
+import java.util.*;
+
+class Node{
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		data = val;
+		lt = rt = null;
+	}
+}
+
 public class P5 {
+	public static Node root;
+	public static void DFS(Node root) {
+		if (root == null) return;
+		else {
+			// System.out.print(root.data + " ");
+			DFS(root.lt);
+			// System.out.print(root.data + " ");
+			DFS(root.rt);
+			System.out.print(root.data + " ");
+		}
+	}
+	
+	public static void main(String []args) {
+		root = new Node(1);
+		root.lt = new Node(2);
+		root.rt = new Node(3);
+		root.lt.lt = new Node(4);
+		root.lt.rt = new Node(5);
+		root.rt.lt = new Node(6);
+		root.rt.rt = new Node(7);
+		DFS(root);
+	}
 }
--- src/inflearnjava/recursivetreegraph/P6.java
+++ src/inflearnjava/recursivetreegraph/P6.java
@@ -1,4 +1,30 @@
 package inflearnjava.recursivetreegraph;
 
+import java.util.*;
+
 public class P6 {
+	
+	static int n;
+	static int[] ch;
+	
+	public static void DFS(int L) {
+		if (L == n + 1) {
+			for (int i = 1; i < n + 1; i++) {
+				if (ch[i] == 1)
+					System.out.print(i + " ");
+			}
+			System.out.println();
+		} else {
+			ch[L] = 1;
+			DFS (L + 1);
+			ch[L] = 0;
+			DFS (L + 1);
+		}
+	}
+	
+	public static void main(String []args) {
+		n = 3;
+		ch = new int[n + 1];
+		DFS(1);
+	}
 }
