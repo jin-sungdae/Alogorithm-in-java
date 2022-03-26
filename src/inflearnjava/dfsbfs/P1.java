fea4bd98e73d3d948cb05a04933ef39eea54a693
Sungdae Jin <sjin@c10r8s9.42seoul.kr>
2022-03-26 15:36:25 +0900

    inflearn dfs

--- src/inflearnjava/dfsbfs/P1.java
+++ src/inflearnjava/dfsbfs/P1.java
@@ -4,18 +4,33 @@ import java.util.*;
 
 public class P1 {
 	static int [] arr;
+	static String answer = "NO";
 	static int [] ch;
-	public static void DFS() {
-		
+	static int total, N;
+	static boolean flag = false;
+	public static void DFS(int L, int sum, int[] arr) {
+		if (flag) return ;
+		if (L == N) {
+			if ((total - sum) == sum) {
+				answer = "YES";
+				flag = true;
+			}
+		} else {
+			DFS(L + 1, sum + arr[L] , arr);
+			DFS(L + 1, sum , arr);
+		}
 	}
 	
 	public static void main(String []args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		N = sc.nextInt();
 		arr = new int[N];
 		ch = new int[N + 1];
 		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
+			total += arr[i];
 		}
+		DFS(0, 0, arr);
+		System.out.println(answer);
 	}
 }
